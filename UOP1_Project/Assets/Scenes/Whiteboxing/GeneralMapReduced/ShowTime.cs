4c2bd550047b355ee532461178933db8df0d37d3
Smurjo <Johanna@johanland.de>
2021-01-26 12:59:15 +0100

    Whiteboxing with reducesd map size
    Scene "GeneralMapReduced"  including new art asset for fallen tree to block path

---
+++ UOP1_Project/Assets/Scenes/Whiteboxing/GeneralMapReduced/ShowTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTime : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Text text = gameObject.GetComponent<Text>();
+		text.text = "Time: " + Time.timeSinceLevelLoad;
+	}
+}
